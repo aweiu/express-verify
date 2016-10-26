9e08f04eb95cfc58e19f750cf20f5540dc3e9bcf
余阿伟 <awei.yu@2zhuzi.com>
2016-10-26 15:00:27 +0800

    修复默认校验不通过提示模版

--- verify-err-msg.js
+++ verify-err-msg.js
@@ -5,28 +5,29 @@
  */
 module.exports = {
   number: {
-    common: '应为数字',
+    common: '{mark}应为数字',
     // >
-    maxNumber: '不能大于{maxNumber}',
+    maxNumber: '{mark}不能大于{maxNumber}',
     // >=
-    maxNumber2: '应小于{maxNumber}',
+    maxNumber2: '{mark}应小于{maxNumber}',
     // <
-    minNumber: '不能小于{minNumber}',
+    minNumber: '{mark}不能小于{minNumber}',
     // <=
-    minNumber2: '应大于{minNumber}',
-    decimalLength: '最多为{decimalLength}位小数'
+    minNumber2: '{mark}应大于{minNumber}',
+    decimalLength: '{mark}最多为{decimalLength}位小数'
   },
   // 特殊类型
-  int: '应为整数',
+  int: '{mark}仅接受整数',
   phone: '手机号不正确',
   idCard: '身份证号不正确',
   bankCard: '银行卡号不正确',
   verifyCode: '验证码错误',
   email: '邮箱格式不正确',
-  // 其他
   common: {
-    length: '内容长度必须为{length}位',
-    minLength: '内容至少{minLength}位',
-    maxLength: '内容至多{maxLength}位'
+    empty: '{mark}不能为空',
+    length: '{mark}必须为{length}位',
+    minLength: '{mark}至少{minLength}位',
+    maxLength: '{mark}至多{maxLength}位',
+    mustBeNull: '{mark}参数不允许'
   }
 };
